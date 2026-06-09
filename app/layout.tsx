aa51fde6f9ad444da12134380fdee044f5f27779
Younghun Kim <kyhkyh0815@gmail.com>
2026-06-10 00:43:37 +0900

    Suppress extension hydration warning

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ko">
+    <html lang="ko" data-scroll-behavior="smooth" suppressHydrationWarning>
       <head>
         <link
           rel="stylesheet"
